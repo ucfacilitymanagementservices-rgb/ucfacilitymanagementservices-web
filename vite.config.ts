54a40cd9b585386328d70148305f5cf1858a7a77
ucfacilitymanagementservices-rgb <ucfacilitymanagementservices@gmail.com>
2026-01-21 22:12:28 +0530

    Simplify Vite config by removing build options
    Removed build configuration and set base URL.

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
+  base: '/',                //
+  plugins:  [react()],
 })
     
